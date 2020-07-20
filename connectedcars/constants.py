29dca7bb33d549dbb4803688032ae3a13b932eba
Jais Heslegrave <jais@heslegrave.dk>
2020-07-20 18:15:49 +0200

    Adding a few more predefined queries into the constants

--- connectedcars/constants.py
+++ connectedcars/constants.py
@@ -4,6 +4,83 @@ API_DEFAULT_TIMEOUT = 30
 
 HEADER_NAMESPACE = "x-organization-namespace"
 
+QUERY_COMPLETE = """
+query User {
+    viewer {
+        id
+        firstname
+        lastname
+        email
+        vehicles {
+        vehicle {
+            id
+            vin
+            class
+            brand
+            make
+            model
+            name
+            licensePlate
+            fuelType
+            fuelLevel {
+                liter
+            }
+            fuelPercentage {
+                percent
+            }
+            odometer {
+                odometer
+            }
+            position {
+                latitude
+                longitude
+            }
+            refuelEvents {
+                litersDifference
+                time
+            }
+            latestBatteryVoltage {
+                voltage
+            }
+            health {
+                ok
+                recommendation
+            }
+            trips (last:3) {
+                items {
+                    duration
+                    fuelUsed
+                    mileage
+                    startLongitude
+                    startLatitude
+                    endLongitude
+                    endLatitude
+                }
+            }
+        }
+        }
+    }
+}"""
+QUERY_USER = """
+query User {
+    viewer {
+        id
+        firstname
+        lastname
+        email
+    }
+}"""
+QUERY_VEHICLE_VIN = """
+query User {
+    viewer {
+        vehicles {
+            vehicle {
+                id
+                vin
+            }
+        }
+    }
+}"""
 QUERY_VEHICLE_OVERVIEW = """
 query User {
     viewer {
